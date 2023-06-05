61d737c16b4edd2f0068c4481ad4aad97e9dd789
pr9njamdar <pr9njamdar@gmail.com>
2023-06-05 10:27:05 +0530

    server

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const express = require('express');
-const WebSocket = require('ws');
+const express = require("express");
+const WebSocket = require("ws");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/templates/index.html");
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+
+var clients = [];
+wss.on("connection", (ws) => {
+  clients.push(ws);
+  console.log(clients.length);
+  clients.forEach((client) => {
+    client.send("A new ws connected");
+  });
+  console.log("Websocket connected");
+
+  ws.on("message", (message) => {
+    const messageString = message.toString("utf8");
+    console.log("Received message from a client", messageString);
+
+    clients.forEach((client) => {
+      client.send(messageString);
+    });
+  });
+
+  ws.on("close", () => {
+    const index = clients.indexOf(ws);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+    console.log("WebSocket connection closed");
+  });
+});
 
 const server = app.listen(3000, () => {
   console.log(`Server is running on 3000`);
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/templates/index.html')
-})
-
-const wss = new WebSocket.Server({ server: server });
-
-var clients=[]
-wss.on('connection', (ws) => {
-  clients.push(ws)
-  console.log(clients.length)
-  clients.forEach((client)=>{
-    client.send('A new ws connected');
-})  
-  console.log('Websocket connected')
-  
-  ws.on('message', (message) => {
-    const messageString = message.toString('utf8');
-    console.log('Received message a client', messageString);
-    
-        clients.forEach((client)=>{
-            client.send(messageString);
-        })  
-   
-   
-   
-     
-  });
-  
-  
-  ws.on('close', () => {
-    const index=clients.indexOf(ws)
-    clients=clients.splice(index,1)
-    console.log('WebSocket connection closed');
-  });
-});
-
-
-
-
-
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
-  });
+server.on("upgrade", (request, socket, head) => {
+  const key = request.headers["sec-websocket-key"];
+  if (key && key === "your_esp32_websocket_key") {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit("connection", ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
 });
