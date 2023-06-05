6f52baa61c4b49dc1d4adb9baa3528eede8cd2b0
pr9njamdar <pr9njamdar@gmail.com>
2023-06-05 11:02:31 +0530

    new

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
-const express = require("express");
-const WebSocket = require("ws");
-const app = express();
+var express=require('express')
+var app =express();
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/templates/index.html");
+var WebSocketServer=require('websocket').server
+var http = require('http')
+
+var server=http.createServer(app);
+
+app.use(express.json());
+
+const Port=80 ;
+
+server.listen(Port,()=>{
+  console.log('listening on port',Port);
 });
 
-const wss = new WebSocket.Server({ noServer: true });
+const wsServer=new WebSocketServer({
+  httpServer:server,
+  autoAcceptConnections:false
+})
 
-var clients = [];
-wss.on("connection", (ws) => {
-  clients.push(ws);
-  console.log(clients.length);
-  clients.forEach((client) => {
-    client.send("A new ws connected");
-  });
-  console.log("Websocket connected");
-
-  ws.on("message", (message) => {
-    const messageString = message.toString("utf8");
-    console.log("Received message from a client", messageString);
-
-    clients.forEach((client) => {
-      client.send(messageString);
-    });
-  });
-
-  ws.on("close", () => {
-    const index = clients.indexOf(ws);
-    if (index !== -1) {
-      clients.splice(index, 1);
-    }
-    console.log("WebSocket connection closed");
-  });
-});
-
-const server = app.listen(3000, () => {
-  console.log(`Server is running on 3000`);
-});
-
-server.on("upgrade", (request, socket, head) => {
-  const key = request.headers["sec-websocket-key"];
-  if (key && key === "your_esp32_websocket_key") {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit("connection", ws, request);
-    });
-  } else {
-    socket.destroy();
-  }
-});
+wsServer.on('request',async(req)=>{
+  var connection = await req.accept(null,req.origin);
+  console.log('Ws connection')
+  connection.on('message',(data)=>{
+    console.log(data.utf8Data);
+  })
+})
