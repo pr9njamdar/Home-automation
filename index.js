e93c4cbea800ce60a370d57dbf4208e75f0ec1b3
pr9njamdar <pr9njamdar@gmail.com>
2023-06-05 11:38:18 +0530

    ok working

--- index.js
+++ index.js
@@ -14,15 +14,25 @@ server.listen(Port,()=>{
   console.log('listening on port',Port);
 });
 
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+'/templates/index.html')
+})
+
 const wsServer=new WebSocketServer({
   httpServer:server,
   autoAcceptConnections:false
 })
-
+var clients=[]
 wsServer.on('request',async(req)=>{
   var connection = await req.accept(null,req.origin);
   console.log('Ws connection')
+  clients.push(connection)
   connection.on('message',(data)=>{
-    console.log(data.utf8Data);
+    clients.forEach((client)=>{
+      client.send(data.utf8Data)
+    })
+  console.log(data.utf8Data);
+
+ 
   })
 })
